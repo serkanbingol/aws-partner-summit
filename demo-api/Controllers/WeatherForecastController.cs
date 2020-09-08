d0f59a6632a9682205b573b13095183c987e6ebb
serkanbingol <serkanbingol@outlook.com>
2020-09-08 06:44:30 +0300

    Test Added

--- demo-api/Controllers/WeatherForecastController.cs
+++ demo-api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace demo_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
